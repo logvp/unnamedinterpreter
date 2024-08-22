7b70ab0ab5249d26c183fc9ad27b817e2dae82ba
Log <lrvanpel@asu.edu>
2024-08-22 15:37:10 -0700

    Use ast visitor in bytecode generator

--- src/bytecode/compiler.rs
+++ src/bytecode/compiler.rs
@@ -1,7 +1,8 @@
 use crate::{
     argp::Options,
-    ast::{Ast, AstNode, Block, Expression, Statement},
+    ast::{Ast, Block, Expression, Statement},
     error::Error,
+    visitor::AstVisitor,
     Symbol,
 };
 
@@ -45,9 +46,7 @@ impl<'a> BytecodeCompiler<'a> {
     ) -> Result<ProgramChunk, Error> {
         TypeChecker::check(&ast, options.unify_branches)?;
         let mut compiler = BytecodeCompiler::new(start_index, variables);
-        for node in ast.nodes.iter() {
-            compiler.compile_node(node)?;
-        }
+        compiler.visit_ast(&ast)?;
         Ok(ProgramChunk {
             starts_at: compiler.start_index,
             procedures: compiler.procedures,
@@ -105,28 +104,19 @@ impl<'a> BytecodeCompiler<'a> {
     fn instruction_index(&self) -> usize {
         self.procedures[*self.procedure_index.last().unwrap()].len()
     }
+}
 
-    fn compile_node(&mut self, node: &AstNode) -> Result<(), Error> {
-        match node {
-            AstNode::Expression(expr) => self.compile_expr(expr),
-            AstNode::Statement(stmt) => self.compile_stmt(stmt),
-        }
-    }
+impl<'a> AstVisitor for BytecodeCompiler<'a> {
+    type Good = ();
+    type Bad = Error;
 
-    fn compile_stmt(&mut self, stmt: &Statement) -> Result<(), Error> {
+    fn visit_statement(&mut self, stmt: &Statement) -> Result<Self::Good, Self::Bad> {
         match stmt {
-            Statement::Assignment(lvalue, expr) => {
-                // if local was declared const, return error
-                let dest = self.resolve(lvalue.name().unwrap());
-                self.compile_expr(expr)?;
-                self.push_instruction(Instruction::Store { dest });
+            Statement::Assignment(lvalue, expr) => self.visit_assignment(lvalue, expr)?,
+            Statement::Declaration(ident, expr, is_const) => {
+                self.visit_declaration(ident, expr, *is_const)?
             }
-            Statement::Declaration(ident, expr, _) => {
-                let dest = self.resolve(ident.name.clone());
-                self.compile_expr(expr)?;
-                self.push_instruction(Instruction::Store { dest });
-            }
-            Statement::Expression(expr) => self.compile_expr(expr)?,
+            Statement::Expression(expr) => self.visit_expr(expr)?,
         }
         self.push_instruction(Instruction::Nullary {
             src: Source::Immediate(Value::None),
@@ -134,162 +124,204 @@ impl<'a> BytecodeCompiler<'a> {
         Ok(())
     }
 
-    fn compile_expr(&mut self, expr: &Expression) -> Result<(), Error> {
-        match expr {
-            Expression::Binary(op, lhs, rhs) => {
-                let mut src0 = match lhs.as_ref() {
-                    Expression::Literal(literal) => Source::Immediate(Value::from(literal)),
-                    Expression::Variable(ident) => self.resolve(ident.name.clone()),
-                    expr => {
-                        self.compile_expr(expr)?;
-                        Source::Result
-                    }
-                };
-                let src1 = match rhs.as_ref() {
-                    Expression::Literal(literal) => Source::Immediate(Value::from(literal)),
-                    Expression::Variable(ident) => self.resolve(ident.name.clone()),
-                    expr => {
-                        // save lhs
-                        if !matches!(src0, Source::Result) {
-                            self.push_instruction(Instruction::Nullary { src: src0 });
-                        }
-                        self.push_instruction(Instruction::Store {
-                            dest: Source::Stack,
-                        });
-                        src0 = Source::Stack;
-                        self.compile_expr(expr)?;
-                        Source::Result
-                    }
-                };
-                self.push_instruction(Instruction::Binary {
-                    op: *op,
-                    src0,
-                    src1,
-                });
-                Ok(())
+    fn visit_binary(
+        &mut self,
+        op: crate::ast::BinaryOperator,
+        lhs: &Expression,
+        rhs: &Expression,
+    ) -> Result<Self::Good, Self::Bad> {
+        let mut src0 = match lhs {
+            Expression::Literal(literal) => Source::Immediate(Value::from(literal)),
+            Expression::Variable(ident) => self.resolve(ident.name.clone()),
+            expr => {
+                self.visit_expr(expr)?;
+                Source::Result
             }
-            Expression::Unary(op, lhs) => {
-                self.compile_expr(lhs)?;
-                self.push_instruction(Instruction::Unary {
-                    op: *op,
-                    src0: Source::Result,
-                });
-                Ok(())
-            }
-            Expression::Literal(literal) => {
-                self.push_instruction(Instruction::Nullary {
-                    src: Source::Immediate(Value::from(literal)),
-                });
-                Ok(())
-            }
-            Expression::Variable(ident) => {
-                let src = self.resolve(ident.name.clone());
-                self.push_instruction(Instruction::Nullary { src });
-                Ok(())
-            }
-            Expression::Block(block) => self.compile_block(block),
-            Expression::IfElse(expr, if_block, else_block) => {
-                self.compile_expr(expr)?;
-                let begin_if_index = self.instruction_index();
-                self.push_instruction(Instruction::Noop);
-                self.compile_block(if_block)?;
-                let end_if_index = self.instruction_index();
-                self.push_instruction(Instruction::Noop);
-                self.compile_block(else_block)?;
-                let end_else_index = self.instruction_index();
-
-                self.patch_instruction(
-                    begin_if_index,
-                    Instruction::JumpFalse {
-                        jump_dest: end_if_index + 1,
-                    },
-                );
-                self.patch_instruction(
-                    end_if_index,
-                    Instruction::UnconditionalJump {
-                        jump_dest: end_else_index,
-                    },
-                );
-                Ok(())
-            }
-            Expression::While(expr, body) => {
-                let continue_index = self.instruction_index();
-                self.compile_expr(expr)?;
-                let condition_jump_index = self.instruction_index();
-                self.push_instruction(Instruction::Noop);
-                self.compile_block(body)?;
-                self.push_instruction(Instruction::UnconditionalJump {
-                    jump_dest: continue_index,
-                });
-                let break_index = self.instruction_index();
-
-                self.patch_instruction(
-                    condition_jump_index,
-                    Instruction::JumpFalse {
-                        jump_dest: break_index,
-                    },
-                );
-                Ok(())
-            }
-            Expression::FunctionCall(fun, arguments) => {
-                for arg in arguments.iter() {
-                    // evaluate arguments in order and push them onto the argument stack
-                    self.compile_expr(arg)?;
-                    self.push_instruction(Instruction::Store {
-                        dest: Source::Stack,
-                    });
+        };
+        let src1 = match rhs {
+            Expression::Literal(literal) => Source::Immediate(Value::from(literal)),
+            Expression::Variable(ident) => self.resolve(ident.name.clone()),
+            expr => {
+                // save lhs
+                if !matches!(src0, Source::Result) {
+                    self.push_instruction(Instruction::Nullary { src: src0 });
                 }
-
-                // call instruction with the function object in result
-                self.compile_expr(fun)?;
-                self.push_instruction(Instruction::Call {
-                    argc: arguments.len(),
+                self.push_instruction(Instruction::Store {
+                    dest: Source::Stack,
                 });
-                Ok(())
+                src0 = Source::Stack;
+                self.visit_expr(expr)?;
+                Source::Result
             }
-            Expression::Lambda(parameters, body) => {
-                // Compile the function body in a new procedure
-                let procedure_id = self.push_procedure();
-                self.push_instruction(Instruction::CreateScope {
-                    locals: parameters.len(),
-                });
-                for param in parameters.iter() {
-                    self.resolve(param.name.clone());
-                }
-                for param in parameters.iter().rev() {
-                    self.push_instruction(Instruction::Nullary { src: Source::Stack });
-                    self.push_instruction(Instruction::Store {
-                        dest: self.resolve(param.name.clone()),
-                    })
-                }
-                self.compile_block(body)?;
-                self.push_instruction(Instruction::DestroyScope {
-                    locals: parameters.len(),
-                });
-                self.pop_procedure();
-                // end the function compilation, now push the function object to the Result
-                self.push_instruction(Instruction::FunctionLiteral {
-                    arity: parameters.len(),
-                    procedure_id,
-                });
-                Ok(())
-            }
-        }
+        };
+        self.push_instruction(Instruction::Binary { op, src0, src1 });
+        Ok(())
     }
 
-    fn compile_block(&mut self, block: &Block) -> Result<(), Error> {
+    fn visit_unary(
+        &mut self,
+        op: crate::ast::UnaryOperator,
+        e: &Expression,
+    ) -> Result<Self::Good, Self::Bad> {
+        self.visit_expr(e)?;
+        self.push_instruction(Instruction::Unary {
+            op,
+            src0: Source::Result,
+        });
+        Ok(())
+    }
+
+    fn visit_literal(&mut self, literal: &crate::ast::Literal) -> Result<Self::Good, Self::Bad> {
+        self.push_instruction(Instruction::Nullary {
+            src: Source::Immediate(Value::from(literal)),
+        });
+        Ok(())
+    }
+
+    fn visit_variable(&mut self, ident: &crate::ast::Identifier) -> Result<Self::Good, Self::Bad> {
+        let src = self.resolve(ident.name.clone());
+        self.push_instruction(Instruction::Nullary { src });
+        Ok(())
+    }
+
+    fn visit_if_else(
+        &mut self,
+        cond: &Expression,
+        if_true: &Block,
+        if_false: &Block,
+    ) -> Result<Self::Good, Self::Bad> {
+        self.visit_expr(cond)?;
+        let begin_if_index = self.instruction_index();
+        self.push_instruction(Instruction::Noop);
+        self.visit_block(if_true)?;
+        let end_if_index = self.instruction_index();
+        self.push_instruction(Instruction::Noop);
+        self.visit_block(if_false)?;
+        let end_else_index = self.instruction_index();
+
+        self.patch_instruction(
+            begin_if_index,
+            Instruction::JumpFalse {
+                jump_dest: end_if_index + 1,
+            },
+        );
+        self.patch_instruction(
+            end_if_index,
+            Instruction::UnconditionalJump {
+                jump_dest: end_else_index,
+            },
+        );
+        Ok(())
+    }
+
+    fn visit_while(&mut self, cond: &Expression, body: &Block) -> Result<Self::Good, Self::Bad> {
+        let continue_index = self.instruction_index();
+        self.visit_expr(cond)?;
+        let condition_jump_index = self.instruction_index();
+        self.push_instruction(Instruction::Noop);
+        self.visit_block(body)?;
+        self.push_instruction(Instruction::UnconditionalJump {
+            jump_dest: continue_index,
+        });
+        let break_index = self.instruction_index();
+
+        self.patch_instruction(
+            condition_jump_index,
+            Instruction::JumpFalse {
+                jump_dest: break_index,
+            },
+        );
+        Ok(())
+    }
+
+    fn visit_function_call(
+        &mut self,
+        func: &Expression,
+        args: &[Expression],
+    ) -> Result<Self::Good, Self::Bad> {
+        for arg in args.iter() {
+            // evaluate arguments in order and push them onto the argument stack
+            self.visit_expr(arg)?;
+            self.push_instruction(Instruction::Store {
+                dest: Source::Stack,
+            });
+        }
+
+        // call instruction with the function object in result
+        self.visit_expr(func)?;
+        self.push_instruction(Instruction::Call { argc: args.len() });
+        Ok(())
+    }
+
+    fn visit_lambda(
+        &mut self,
+        params: &[crate::ast::Identifier],
+        body: &Block,
+    ) -> Result<Self::Good, Self::Bad> {
+        // Compile the function body in a new procedure
+        let procedure_id = self.push_procedure();
+        self.push_instruction(Instruction::CreateScope {
+            locals: params.len(),
+        });
+        for param in params.iter() {
+            self.resolve(param.name.clone());
+        }
+        for param in params.iter().rev() {
+            self.push_instruction(Instruction::Nullary { src: Source::Stack });
+            self.push_instruction(Instruction::Store {
+                dest: self.resolve(param.name.clone()),
+            })
+        }
+        self.visit_block(body)?;
+        self.push_instruction(Instruction::DestroyScope {
+            locals: params.len(),
+        });
+        self.pop_procedure();
+        // end the function compilation, now push the function object to the Result
+        self.push_instruction(Instruction::FunctionLiteral {
+            arity: params.len(),
+            procedure_id,
+        });
+        Ok(())
+    }
+
+    fn visit_block(&mut self, block: &Block) -> Result<Self::Good, Self::Bad> {
         let Block(nodes) = block;
         // new block = new scope
         self.push_scope();
         let start_index = self.instruction_index();
         self.push_instruction(Instruction::Noop); // Placeholder for CreateScope because number of locals is unknown
         for node in nodes.iter() {
-            self.compile_node(node)?;
+            self.visit_node(node)?;
         }
         let locals = self.variables.get_num_locals_in_scope(self.current_scope);
         self.patch_instruction(start_index, Instruction::CreateScope { locals });
         self.push_instruction(Instruction::DestroyScope { locals }); // could be omitted if locals == 0
         self.pop_scope(); // assert scope was still on stack
+        Ok(())
+    }
+
+    fn visit_assignment(
+        &mut self,
+        lvalue: &crate::ast::Lvalue,
+        expr: &Expression,
+    ) -> Result<Self::Good, Self::Bad> {
+        // if local was declared const, return error
+        let dest = self.resolve(lvalue.name().unwrap());
+        self.visit_expr(expr)?;
+        self.push_instruction(Instruction::Store { dest });
+        Ok(())
+    }
+
+    fn visit_declaration(
+        &mut self,
+        ident: &crate::ast::Identifier,
+        expr: &Expression,
+        _const: bool,
+    ) -> Result<Self::Good, Self::Bad> {
+        let dest = self.resolve(ident.name.clone());
+        self.visit_expr(expr)?;
+        self.push_instruction(Instruction::Store { dest });
         Ok(())
     }
 }
