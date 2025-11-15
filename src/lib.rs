1676686d9119789e4cd9b1d3045fb58c7f2c6919
log <lrvanpel@asu.edu>
2025-11-15 15:11:17 -0800

    Add additional lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+#![deny(
+    unsafe_code,
+    clippy::correctness,
+    clippy::suspicious,
+    unused_must_use,
+    unfulfilled_lint_expectations
+)]
+#![warn(clippy::complexity, clippy::perf, clippy::style)]
+#![warn(clippy::pedantic)]
+#![allow(
+    clippy::missing_panics_doc,
+    clippy::wildcard_imports,
+    clippy::semicolon_if_nothing_returned,
+    clippy::uninlined_format_args,
+    clippy::missing_errors_doc,
+    clippy::match_same_arms,
+    clippy::must_use_candidate
+)]
+
 mod ast;
 pub mod bytecode;
 pub mod error;
